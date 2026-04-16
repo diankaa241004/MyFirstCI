4e51170c7abc9b5e49e0a488f9fb920ca91d91f1
diana <dianaa_sidorova2018@mail.ru>
2026-04-16 13:01:57 +0300

    Initial commit : Solution with web API and tests

---
+++ MyFirstCI.Api/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.IgnoreNullValues = true;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.MapControllers();
+
+app.Run();
---
+++ MyFirstCI.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using MyFirstCI.Api.Controllers;
+
+namespace MyFirstCI.Tests;
+
+public class WeatherForecastControllerTests
+{
+    [Fact]
+    public void AllForecastsInFuture()
+    {
+        var controller = new WeatherController();
+        var result = controller.Get();
+        foreach (var forecast in result)
+            Assert.True(forecast.Date >= DateOnly.FromDateTime(DateTime.Now));
+    }
+
+    [Fact]
+    public void Get_ReturnsFiveForecasts()
+    {
+        var controller = new WeatherController();
+        var result = controller.Get();
+        Assert.Equal(5, result.Count());
+    }
+}
